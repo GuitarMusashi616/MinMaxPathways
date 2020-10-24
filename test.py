e022e384e1314bc484f28f5688ebbcb6de33149c
Austin Williams <amwilliams24@alaska.edu>
2020-10-23 20:30:10 -0800

    more testing

--- test.py
+++ test.py
@@ -3,15 +3,18 @@ import time
 
 
 def test_new_no_grid():
-    grid = generate_grid(5, 5)
+    grid = generate_grid(4, 0)
     print(grid)
     t1 = time.perf_counter()
     print(minmax(grid))
     t2 = time.perf_counter()
     print(alpha_beta(grid))
     t3 = time.perf_counter()
+    print(alpha_beta(grid, depth_limit=5))
+    t4 = time.perf_counter()
     print(t2-t1)
     print(t3-t2)
+    print(t4-t3)
     print(grid)
 
 
